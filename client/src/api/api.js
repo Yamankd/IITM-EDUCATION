dfc437437829f80d2c0bfedb589f14b829c47c70
RohitM <rohitmahor022@gmail.com>
2026-01-01 19:31:04 +0530

    remove process.env

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // automatically picks env
+  baseURL:  import.meta.env.VITE_API_URL, // automatically picks env
   withCredentials: true, // cookies/auth
   headers: {
     "Content-Type": "application/json",
