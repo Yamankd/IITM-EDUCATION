d5ca3334bc01d015bcf8be8d0de049a5f79837d7
RohitM <rohitmahor022@gmail.com>
2026-01-01 19:48:24 +0530

    final push

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL, // automatically picks env
+  baseURL:  "https://iitm-education.onrender.com/iitm", // automatically picks env
   withCredentials: true, // cookies/auth
   headers: {
     "Content-Type": "application/json",
