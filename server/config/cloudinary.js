87a33f7be151b3234e0c3d23837603b4e8e973fb
YamankD <yamankdhryia679@gmail.com>
2026-01-29 18:46:20 +0530

    fixed cloudinary config file.

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'dwuhae5u1',
-    api_key: '751959527126953',
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET // Ensure this is set in your .env file
 });
 
